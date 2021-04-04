03ecf771418c476348e4ec279df83270d04ef390
Gabriel <gabriel.araujo.5000@gmail.com>
2021-04-04 19:43:35 -0300

    Add new hospital models

--- morse_hospital_sim/default.py
+++ morse_hospital_sim/default.py
@@ -10,7 +10,7 @@ from turtlebot_hospital_sim.Turtlebot import Turtlebot
 PATH = "/".join(__file__.split("/")[:-1])
 
 robot1 = Turtlebot(name='turtlebot1')
-robot1.add_to_simulation()
+robot1.add_to_simulation(x=-2, y=-2)
 
 # Clock
 clock = Clock()
@@ -26,7 +26,7 @@ charging_zone.size=[5 for x in range(3)]
 charging_zone.translate(x=-19, y=-3, z=0)
 
 # set 'fastmode' to True to switch to wireframe mode
-env = Environment(f'{PATH}/models/cic.blend', fastmode=False)
+env = Environment(f'{PATH}/models/hospital_v3_v1.blend', fastmode=False)
 env.set_horizon_color(color=(0.65, 0.65, 0.65))
 env.set_camera_location([-30.0, -10, 5.0])
 env.set_camera_rotation([1.09, 0, -0.7])
