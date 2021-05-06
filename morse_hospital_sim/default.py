305fc3d221b010ce71898804404accf83e3c762d
Gabriel <gabriel.araujo.5000@gmail.com>
2021-05-06 20:59:25 -0300

    Load environment variables

--- morse_hospital_sim/default.py
+++ morse_hospital_sim/default.py
@@ -5,25 +5,42 @@
 
 from morse.builder import *
 from turtlebot_hospital_sim.Turtlebot import Turtlebot
-
+from turtlebot_hospital_sim.GrabberRobot import GrabberRobot
+from turtlebot_hospital_sim.Nurse import Nurse
+import os
 
 PATH = "/".join(__file__.split("/")[:-1])
 
-robot1 = Turtlebot(name='turtlebot1')
-robot1.add_to_simulation(x=-2, y=-2)
+# robot_pose_1 = os.environ['ROBOT_POSE_1'][1:-1].split(';')
+# robot1 = GrabberRobot(name='turtlebot1', path=f"{PATH}/models/turtlebot.blend")
+# robot1.add_to_simulation(x=float(robot_pose_1[0]), y=float(robot_pose_1[1]))
+
+
+# robot_pose_2 = os.environ['ROBOT_POSE_2'][1:-1].split(';')
+# robot2 = GrabberRobot(name='turtlebot2', path=f"{PATH}/models/turtlebot.blend")
+# robot2.add_to_simulation(x=float(robot_pose_2[0]), y=float(robot_pose_2[1]))
+
+n_robots = int(os.environ['N_ROBOTS'])
+robot_list = []
+for i in range(1, n_robots+1):
+	robot_name = os.environ['ROBOT_NAME_'+str(i)]
+	robot_pose = os.environ['ROBOT_POSE_'+str(i)][1:-1].split(';')
+	robot = GrabberRobot(name=robot_name, path=f"{PATH}/models/turtlebot.blend")
+	robot.add_to_simulation(x=float(robot_pose[0]), y=float(robot_pose[1]))
+	robot_list.append(robot)
 
 # Clock
 clock = Clock()
-robot1.append(clock)
+robot_list[0].append(clock)
 clock.add_interface('ros', topic="/clock")
-
-robot2 = Turtlebot(name='turtlebot2')
-robot2.add_to_simulation(x=-25)
 
 # Charging Zone
 charging_zone = Zone(type='Charging')
 charging_zone.size=[5 for x in range(3)]
 charging_zone.translate(x=-19, y=-3, z=0)
+
+nurse = Nurse(name='nurse')
+nurse.translate(x=15, y=15, z=0)
 
 # set 'fastmode' to True to switch to wireframe mode
 env = Environment(f'{PATH}/models/hospital_v3_v1.blend', fastmode=True)
@@ -35,3 +52,7 @@ env.use_vsync('OFF')
 env.use_internal_syncer()
 env.set_camera_location([-2, -2, 5.0])
 env.set_camera_rotation([1.09, 0, -0.7])
+
+print('================================================')
+print(os.environ['ROBOT_POSE_1'][1:-1].split(';'))
+print('================================================')
